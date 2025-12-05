d78e807beb181364bc65236e55feff19aeeb8c55
RUCHA LOKHANDE <ruchalokhande09@gmail.com>
2025-12-05 17:51:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  project = var.project
-  region  = var.region
-  zone    = var.zone
-  credentials = file(var.credentials_file)   # read from variable
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+  credentials = file(var.credentials_file)
 }
