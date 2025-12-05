ec0703a874aeb1fe060edeece80a51c9864fcb37
RUCHA LOKHANDE <ruchalokhande09@gmail.com>
2025-12-05 16:16:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "vm" {
+  name         = "jenkins-tf-VM"
+  machine_type = "e2-small"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
